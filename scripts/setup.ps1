f1ba22daed49be1739a74e5bc19cf625bd3ba4a6
AntranigSameen <166274087+AntranigSameen@users.noreply.github.com>
2026-02-07 16:36:29 -0800

    Updated Script
    Should Auto install dependencies

---
+++ scripts/setup.ps1
@@ -0,0 +1,19 @@
+# Stop if anything fails
+$ErrorActionPreference = "Stop"
+
+Write-Host "Creating virtual environment..."
+python -m venv .venv
+
+Write-Host "Activating virtual environment..."
+& .\.venv\Scripts\Activate.ps1
+
+Write-Host "Upgrading pip..."
+python -m pip install --upgrade pip
+
+Write-Host "Installing packages..."
+pip install msal requests python-dotenv
+pip install gspread google-auth google-auth-oauthlib google-auth-httplib2
+pip install imapclient
+pip install pyzmail36
+
+Write-Host "Setup complete ✅"
